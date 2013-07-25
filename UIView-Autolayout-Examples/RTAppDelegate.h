5c283eef468d7d453f9f0c3fdc3d1af18c41d53e
Rajiev Timal <rajiev@fuzzproductions.com>
2013-07-24 20:31:29 -0400

    Initial Commit

---
+++ UIView-Autolayout-Examples/RTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RTAppDelegate.h
+//  UIView-Autolayout-Examples
+//
+//  Created by Rajiev Timal on 7/24/13.
+//  Copyright (c) 2013 Rajiev Timal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIView-Autolayout-ExamplesTests/UIView_Autolayout_ExamplesTests.m
@@ -0,0 +1,32 @@
+//
+//  UIView_Autolayout_ExamplesTests.m
+//  UIView-Autolayout-ExamplesTests
+//
+//  Created by Rajiev Timal on 7/24/13.
+//  Copyright (c) 2013 Rajiev Timal. All rights reserved.
+//
+
+#import "UIView_Autolayout_ExamplesTests.h"
+
+@implementation UIView_Autolayout_ExamplesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in UIView-Autolayout-ExamplesTests");
+}
+
+@end
